3e4be8e50c95d827b714533f1ee8bbcea593b1c2
Tomasz Bazan <tomasz.bazan.89@gmail.com>
2025-01-21 22:08:57 +0100

    Init commit

--- src/main/java/pl/btsoftware/wheresmymoney/WheresMyMoneyApplication.java
+++ src/main/java/pl/btsoftware/wheresmymoney/WheresMyMoneyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WheresmymoneyApplication {
+public class WheresMyMoneyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WheresmymoneyApplication.class, args);
+		SpringApplication.run(WheresMyMoneyApplication.class, args);
 	}
 
 }
---
+++ src/main/java/pl/btsoftware/wheresmymoney/expense/api/ExpenseRequest.java
@@ -0,0 +1,6 @@
+package pl.btsoftware.wheresmymoney.expense.api;
+
+import java.util.UUID;
+
+public record ExpenseRequest(UUID id) {
+}
