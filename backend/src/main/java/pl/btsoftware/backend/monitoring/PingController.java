3a1f7979e6c940f7c19af424c26ab4cc49604fda
Tomasz Bazan <tomasz.bazan.89@gmail.com>
2025-07-21 22:49:36 +0200

    Add PingController with ping endpoint and corresponding tests

---
+++ backend/src/main/java/pl/btsoftware/backend/monitoring/PingController.java
@@ -0,0 +1,15 @@
+package pl.btsoftware.backend.monitoring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
